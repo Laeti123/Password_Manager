ac8a59b0eecd80c3e35247344ee0319f650f5241
Laetitia <you@example.com>
2022-02-27 15:00:55 +0100

    added database logic

--- passwordManager.py
+++ passwordManager.py
@@ -1,3 +1,31 @@
+import psycopg2
+from psycopg2 import Error
+
+try:
+    # Connect to an existing database
+    connection = psycopg2.connect(user="postgres",
+                                  password="123",
+                                  host="localhost",
+                                  port="5433",
+                                  database="password")
+
+    # Create a cursor to perform database operations
+    cursor = connection.cursor()
+
+    # Executing a SQL query
+    cursor.execute("SELECT * FROM mypasswords")
+    # Fetch result
+    record = cursor.fetchone()
+    print("You are connected to - ", record, "\n")
+
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
+
 
 password = input("What is the master password?")
 
